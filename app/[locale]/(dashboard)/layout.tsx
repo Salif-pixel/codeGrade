83a15dcecb3b407841758fefa59acebf4221072e
Salif Biaye <salifbiaye@esp.sn>
2025-03-13 02:28:01 +0000

    update layoutnavigation

--- app/[locale]/(dashboard)/layout.tsx
+++ app/[locale]/(dashboard)/layout.tsx
@@ -2,26 +2,33 @@
 import { ReactNode } from 'react';
 
 import Header from "@/components/dashboard/header/header";
+import {headers} from "next/headers";
+import {auth} from "@/lib/auth";
+import {get_user} from "@/lib/data";
+import NavigationLayout from "@/components/dashboard/header/navigationlayout";
 
 
 interface LayoutProps {
     children: ReactNode;
 }
 
-export default  function Layout({ children }: LayoutProps) {
+export default  async function Layout({ children }: LayoutProps) {
 
-
+    const headersValue = await headers();
+    const sessionPromise = await auth.api.getSession({ headers: headersValue });
+    const user = await get_user(sessionPromise?.user?.id ?? "")
+    if(!user ) {
+        return null;
+    }
     return (
-        <div className="flex flex-col min-h-screen">
-            {/* Navbar */}
-           <Header/>
 
-            {/* Main content */}
+    <NavigationLayout user={user}>
+        <div className="flex flex-col min-h-screen">
             <main className="flex-1 container mx-auto p-4">
                 {children}
             </main>
-
-
         </div>
+    </NavigationLayout>
+
     );
 }
