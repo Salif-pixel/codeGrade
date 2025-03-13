c0083a0c98eb3413e2740b52139b3dbddc274804
Mouhamadou Tidiane Seck <mtseckdev@gmail.com>
2025-03-13 04:21:00 +0000

    nav dynamic

---
+++ components/navigation/Navigation.tsx
@@ -0,0 +1,52 @@
+"use client"
+
+import { navigationConfig } from "@/lib/nav-config";
+import { cn } from "@/lib/utils";
+import { Link } from "@/src/i18n/navigation";
+import { Role } from "@prisma/client";
+import { Badge } from "../ui/badge";
+import { useState } from "react";
+
+const Navigation = ({ userRole }: { userRole: Role }) => {
+    const filteredNavigation = navigationConfig.filter((item) =>
+        item.roles.includes(userRole) || item.roles.length === 0
+    );
+
+    const [activeTab, setActiveTab] = useState("dashboard");
+
+    return (
+        <div className="flex flex-col md:flex-row -mb-px">
+            {filteredNavigation.map((item) => (
+                <Link key={item.id} className="cursor-pointer" href={item.href}>
+                    <button
+                        onClick={() => setActiveTab(item.id)}
+                        className={cn(
+                            "relative text-sm py-3 px-4 flex items-center gap-2 border-b-2 font-medium transition-all",
+                            activeTab === item.id
+                                ? "border-primary text-primary"
+                                : "border-transparent text-muted-foreground hover:text-foreground hover:bg-muted/20",
+                        )}
+                    >
+                        {item.icon}
+                        <span>{item.title}</span>
+                        {item.badge && (
+                            <Badge
+                                variant={activeTab === item.id ? "default" : "secondary"}
+                                className="ml-1 h-5 min-w-5 px-1 flex items-center justify-center"
+                            >
+                                {item.badge}
+                            </Badge>
+                        )}
+
+                        {/* Active indicator dot */}
+                        {activeTab === item.id && (
+                            <span className="absolute -bottom-[1.5px] left-1/2 transform -translate-x-1/2 w-1.5 h-1.5 rounded-full bg-primary" />
+                        )}
+                    </button>
+                </Link>
+            ))}
+        </div>
+    );
+};
+
+export default Navigation;
